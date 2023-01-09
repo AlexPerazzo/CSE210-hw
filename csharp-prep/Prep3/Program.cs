41de2ecee546af4c21dd57559a24033eee2b868e
Alex Perazzo <theodoreperazzo@byui.edu>
2023-01-09 14:12:27 -0700

    Finished Prep3 and stretch challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,31 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What's the magic number? ");
-        string userInput = Console.ReadLine();
-        int magicNumber = int.Parse(userInput);
-        int guess;
-
+        string answer;
         do
         {
-            Console.Write("What's your guess? ");
-            userInput = Console.ReadLine();
-            guess = int.Parse(userInput);
+            // Console.Write("What's the magic number? ");
+            // string userInput = Console.ReadLine();
+            Random randomGenerator = new Random();
             
-            if (guess > magicNumber)
-            {
-                Console.WriteLine("Too High");
-            }
+            // int magicNumber = int.Parse(userInput);
+            int magicNumber = randomGenerator.Next(0, 11);
+            int guess;
+            string userInput;
+            int count = 0;
 
-            if (guess < magicNumber)
+            do
             {
-                Console.WriteLine("Too Low");
-            }
+                Console.Write("What's your guess? ");
+                userInput = Console.ReadLine();
+                guess = int.Parse(userInput);
+                count ++;
 
-            if (guess == magicNumber)
-            {
-                Console.WriteLine("Correct!");
-            }
-        } while (guess != magicNumber);
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Too High");
+                }
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Too Low");
+                }
+
+                if (guess == magicNumber)
+                {
+                    Console.WriteLine("Correct!");
+                    Console.WriteLine($"It took you {count} tries");
+                }
+
+
+            } while (guess != magicNumber);
+            Console.Write("Would you like to play again? ");
+            answer = Console.ReadLine();
+        } while (answer == "yes");
     }
 }
