13531059f01b6d19db6acd7c709d5d2852c45fda
Alex Perazzo <theodoreperazzo@byui.edu>
2023-01-09 12:30:59 -0700

    Started Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What's the magic number? ");
+        string userInput = Console.ReadLine();
+        int magicNumber = int.Parse(userInput);
+        int guess;
+
+        do
+        {
+            Console.Write("What's your guess? ");
+            userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+            
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("Correct!");
+            }
+        } while (guess != magicNumber);
     }
 }
