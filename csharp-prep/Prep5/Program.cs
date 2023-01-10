4a11a80002ab863291bee43b5b18dbe21c82f8d0
Alex Perazzo <theodoreperazzo@byui.edu>
2023-01-09 18:55:42 -0700

    Finished Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        string username = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int square = SquareNumber(favNumber);
+        DisplayResult(square, username);
+
+
+        
     }
+
+    static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(int number, string name)
+    {
+        Console.WriteLine($"{name}, your favorite number squared is {number}.");
+        
+    }
+
 }
