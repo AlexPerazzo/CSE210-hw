7a568dd978569134fb8b81b0129d7e9c10db9f3f
Alex Perazzo <theodoreperazzo@byui.edu>
2023-01-09 17:12:59 -0700

    Finished Prep 4 + streches

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,64 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        List<int> numbers = new List<int>();
+        List<int> minList = new List<int>();
+        int number;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            
+        
+        string userInput = Console.ReadLine();
+        number = int.Parse(userInput);
+
+
+        if (number != 0)
+        {
+        numbers.Add(number);
+        }
+
+        if (number > 0)
+        {
+            minList.Add(number);
+        }
+
+        } while (number != 0);
+        
+        int total = 0;
+        foreach (int i in numbers)
+        {
+            total += i;
+        }
+
+        int count = numbers.Count;
+
+        int average;
+
+        average = total / count;
+
+        int max = numbers.Max();
+
+        int min = minList.Min();
+
+        Console.WriteLine(total);
+        Console.WriteLine(average);
+        Console.WriteLine(max);
+        Console.WriteLine(min);
+
+        numbers.Sort();
+
+        foreach (int x in numbers)
+        {
+            Console.WriteLine("Sorted List:");
+            Console.WriteLine(x);
+        }
     }
 }
