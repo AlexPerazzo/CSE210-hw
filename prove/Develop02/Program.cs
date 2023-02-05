6b4345c59cf2631d43d4343cd1ff190e84e78fd2
Alex Perazzo <theodoreperazzo@byui.edu>
2023-02-05 11:59:01 -0700

    Added comment on what I did for above and beyond

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -3,7 +3,7 @@ using Develop02.Classes;
 class Program
 {
 
-
+// For the above and beyond I added a start up menu that allows you to load files before you go into the actual program. I feel like this helps with the flow of the program a lot. I also added text to various places to make sure the user knows if they are overwriting files and such.
 
 
 
